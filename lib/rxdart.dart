21ff2fb6c2ebf1b8307a38c2875bb9e4d28f5146
gekkotadev <114091010+GekkotaDev@users.noreply.github.com>
2023-06-22 13:07:12 +0800

    Added computed API

--- lib/rxdart.dart
+++ lib/rxdart.dart
@@ -1,3 +1,4 @@
+/// Exports the underlying RxDart package used by `rxs`.
 library rxdart;
 
 export "package:rxdart/rxdart.dart";
--- lib/rxs.dart
+++ lib/rxs.dart
@@ -1,7 +1,7 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// Compose states and streams with ease, inspired by Solid.js and the concept
+/// of signals.
 library rxs;
 
 export "src/effects.dart";
+export "src/memos.dart";
 export "src/signals.dart";
